98da6a3ffff4bc693991c664909b18a12f666efe
hanjoon-kim <hanjoon3@gmail.com>
2018-07-05 19:09:14 -0700

    Implement functional CRUDy app

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
+
+const readFilePromise = Promise.promisify(fs.readFile);
 
 var items = {};
 
@@ -21,48 +24,61 @@ exports.create = (text, callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  
-  // fs.readFile(exports.dataDir) {
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
 
-  // }
-
-
-
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, {id: id, text: item});
-  }
+  fs.readFile(filepath, (err, text) => {
+    if (err) {
+      callback(new Error( `No item with id: ${id}`));
+    } else {
+      callback(null, {id, text: text.toString()});
+    }
+  });
 };
 
 exports.readAll = (callback) => {
-  var data = [];
-  _.each(items, (item, idx) => {
-    data.push({ id: idx, text: items[idx] });
+  
+  fs.readdir(exports.dataDir, (err, files) => {
+    var data = _.map(files, (file) => {
+      var id = path.basename(file, '.txt');
+      var filepath = path.join(exports.dataDir, `${id}.txt`);
+
+      return readFilePromise(filepath).then((data) => {
+        return {id: id, text: data.toString()};
+      });
+    });
+    Promise.all(data).then((items) => callback(null, items));
   });
-  callback(null, data);
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, {id: id, text: text});
-  }
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+  
+  fs.access(filepath, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(filepath, text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, {id, text});
+        }
+      });     
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if(!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`))
-  } else {
-    callback();
-  }
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+
+  fs.unlink(filepath, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
+  
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
--- test/test.js
+++ test/test.js
@@ -114,23 +114,6 @@ describe('todos', () => {
         done();
       });
     });
-
-    // Refactor this test when completing `readAll`
-    it('should return an array with all saved todos', (done) => {
-      const todo1text = 'todo 1'
-      const todo2text = 'todo 2'
-      const expectedTodoList = [{ id: '00001', text: '00001' }, { id: '00002', text: '00002' }];
-      todos.create(todo1text, (err, todo) => {
-        todos.create(todo2text, (err, todo) => {
-          todos.readAll((err, todoList) => {
-            expect(todoList).to.have.lengthOf(2);
-            expect(todoList).to.deep.include.members(expectedTodoList, 'NOTE: Text field should use the Id initially');
-            done();
-          });
-        });
-      });
-    });
-
   });
 
   describe('readOne', () => {
@@ -183,8 +166,8 @@ describe('todos', () => {
         expect(currentTodoCount).to.equal(initalTodoCount);
         expect(err).to.exist;
         done();
-      })
-    })
+      });
+    });
   });
 
   describe('delete', () => {
